dc1dcf9b45feb4854e6ab0ca4228b62e86ac8231
Teresa Kałmuczak <t.sembratowicz@gmail.com>
2024-01-13 21:24:49 +0100

    implemented choosing user gender

--- src/services/user_generator.py
+++ src/services/user_generator.py
@@ -1,14 +1,15 @@
 import requests
-import json
 from src.models.user import User
 
 base_path = "https://randomuser.me/api"
 
 def generate_user(gender=None):
-    if (gender == None):
-        user = requests.get(base_path).json()['results'][0]
-        first_name = user['name']['first']
-        last_name = user['name']['last']
-        return User(first_name + ' ' + last_name)
-    else:
-        return None
+    path = base_path
+    if gender!=None:
+        path = path + '/?gender=' + gender
+
+    user = requests.get(path).json()['results'][0]
+    first_name = user['name']['first']
+    last_name = user['name']['last']
+    gender = user['gender']
+    return User(first_name + ' ' + last_name, gender)
